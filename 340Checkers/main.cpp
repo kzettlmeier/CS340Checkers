c5213f130db9daa037bb57dfd2147e865e7ffc2d
kzettlmeier <kzettlmeier@sbcglobal.net>
2013-10-03 22:49:54 -0500

    Added the board object

--- 340Checkers/main.cpp
+++ 340Checkers/main.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include "board.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    Board board;
+    board.displayBoard();
 
     return a.exec();
 }
